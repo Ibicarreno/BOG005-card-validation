a859e9c5451d258bbf959c8b4da9110064bd6b1f
Ibicarreno <you@example.comibi.carreno@gmail.com>
2022-07-21 11:07:37 -0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+function validateForm(){
+    var cardNumberInput= document.getElementById("cardNumberId");
+    var monthInput= document.getElementById("monthId");
+    var yearInput= document.getElementById("yearId");
+    var ccvInput= document.getElementById("ccvId");
+
+    if (cardNumberInput.checkValidity()==false){
+        return false;
+    }
+    if (monthInput.checkValidity()==false){
+        return false;
+    }
+    if (yearInput.checkValidity()==false){
+        return false;
+    }
+    if (ccvInput.checkValidity()==false){
+        return false;
+    }
+    return true;
+}
+
+window.validateCard = function validateCard(){
+
+    if (validateForm()==true){
+        var inputCardNumber = document.getElementById("cardNumberId").value;
+        var cardStatus = validator.validationCard(inputCardNumber);
+        var maskChange= validator.maskify(inputCardNumber);
+        var message;
+        if (cardStatus==true){
+            message = "Tu tarjeta "+ maskChange + " es valida";
+        }
+        else{
+            message = "Tu tarjeta " + maskChange + " es invalida";
+        }
+        document.getElementById("cardStatusId").innerHTML=message;
+    }
+    else{
+        document.getElementById("cardStatusId").innerHTML="Digita tus datos";
+    }
+    document.getElementById("cardNumberId").value=maskChange;
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,64 @@
 const validator = {
-  // ...
+  validationCard,
+  maskify,
 };
+
+/*function maskify(maskCreditNumber){
+  let maskString = maskCreditNumber.replace(/.(?=.{4})/g, "#");
+
+  return maskString;
+}*/
+
+function maskify(numberCard){
+  var maskifyNumber = numberCard;
+  var finalNumber = '';
+  for (var i = 0; i < maskifyNumber.length; i++) {
+    if (i < maskifyNumber.length - 4) {
+      finalNumber = finalNumber + '#';
+    } else {
+      finalNumber = finalNumber + (maskifyNumber[i]); 
+    }     
+  }
+  return finalNumber;
+}
+
+function validationCard (cardNumber){ /* Funcion para la validacion del numero de tarjeta ingresada*/ 
+  if(cardNumber.length==16){ /* If que condiciones a que el numero de tarjeta tenga un largo de 16 digitos */
+    var arrayCardNumber = cardNumber.split(""); /** Se convierte el string en un array utilizando .split */
+    var reverseArrayCardNumber = arrayCardNumber.reverse(); /** Se invierten los numeros del array con .reverse */
+    var sumNumbers=0; /** se crea variable que contenga todos las cantidades finales de los pares y los impares con el algoritmo de luhn */
+    
+    for(var i=0; i<reverseArrayCardNumber.length; i++){ /** Ciclo for para que cuente desde la posicion 0 hasta el largo de la tajeta de credito */
+      var operateNumber; /** se crea variable para hacer las respectivas operaciones */
+      if(i%2==0){ /** Condicional para que selecciones las posiciones pares  */
+        operateNumber = reverseArrayCardNumber[i]; /** En esta variable se guardan los numeros pares del array */
+      }
+      else {
+        var multipliedByTwo = reverseArrayCardNumber[i]*2; /** Los numeros impares se multiplican por 2 */
+        var multipliedByTwoString = multipliedByTwo.toString(); /** El resultado de la operacion anterior se convierte a string */
+        var arrayMultipliedByTwoString = multipliedByTwoString.split(""); /** El string anteior se convierte en array */
+
+        operateNumber = 0;
+        for (var j=0; j<arrayMultipliedByTwoString.length; j++){ /** Ciclo for para que se sumen los digitos de cada uno de los numeros impares */
+          operateNumber=operateNumber+ parseInt(arrayMultipliedByTwoString[j]); 
+         
+        }
+      }
+      sumNumbers=sumNumbers + parseInt(operateNumber);
+    }
+    if (sumNumbers%10 ==0){
+      return true;
+    }
+    else{
+      return false;
+    }
+
+
+  }
+  else {
+    return false;
+  }
+}
+
 
 export default validator;
